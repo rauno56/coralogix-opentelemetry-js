2c661c7e3e7c427ee515468645ad982c8a097419
Gal Rosenberg <59124444+galrose@users.noreply.github.com>
2024-07-29 12:47:05 +0300

    Task/express (#19)
    This pull request includes several updates to the CI workflow,
    dependencies, and enhancements to the `CoralogixTransactionSampler`
    class. The most important changes include upgrading GitHub Actions
    versions, modifying the test script in `package.json`, adding new type
    definitions, and enhancing the transaction sampler to support Express
    routes.
    
    ### CI Workflow and Dependencies:
    
    *
    [`.github/workflows/ci.yml`](diffhunk://#diff-b803fcb7f17ed9235f1e5cb1fcd2f5d3b2838429d4368ae4c57ce4436577f03fL11-R12):
    Updated GitHub Actions versions for `checkout` and `setup-node` to v4.
    *
    [`package.json`](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519L12-R12):
    Updated `tsx` dependency version and added `@types/express` to
    `devDependencies`.
    [[1]](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519L12-R12)
    [[2]](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519R22)
    [[3]](diffhunk://#diff-7ae45ad102eab3b6d7e7896acd08c427a9b25b346470d7bc6507b6481575d519L30-R31)
    
    ### Enhancements to `CoralogixTransactionSampler`:
    
    *
    [`src/trace/samplers/coralogix-transaction-sampler.ts`](diffhunk://#diff-17a39904e9223a0a059c6dde37c3aab37e7bde031fd8196d821f7f28e9445daeR5-R30):
    Added support for Express routes by introducing new interfaces
    (`RouteMapping`, `Stack`, `Handle`, `Handler`) and methods
    (`setExpressApp`, `_getPathFromRoutes`,
    `_buildTransactionNameFromExpressPath`, `isMiddlewareILayer`,
    `isMiddlewareHandler`).
    [[1]](diffhunk://#diff-17a39904e9223a0a059c6dde37c3aab37e7bde031fd8196d821f7f28e9445daeR5-R30)
    [[2]](diffhunk://#diff-17a39904e9223a0a059c6dde37c3aab37e7bde031fd8196d821f7f28e9445daeR43-R107)
    [[3]](diffhunk://#diff-17a39904e9223a0a059c6dde37c3aab37e7bde031fd8196d821f7f28e9445daeL48-L60)

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Coralogix Opentelemetry
+
+## Flows With Express
+To use coralogix flows with express you must use the `addExpressApp`
+function to make sure that the `coralogix sampler` understands
+your routes and endpoints.
+
+Example:
+
+```javascript
+const sampler = new CoralogixTransactionSampler();
+
+const tracerProvider = new BasicTracerProvider({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: '<your-service-name>'
+    }),
+    sampler
+});
+import express from "express";
+import router from "./router";
+
+const app = express();
+app.use('/', router);
+
+sampler.setExpressApp(app);
+
+app.listen(3000, () => {
+    console.log('Server is running')
+});
--- src/trace/samplers/coralogix-transaction-sampler.ts
+++ src/trace/samplers/coralogix-transaction-sampler.ts
@@ -2,10 +2,33 @@ import {AlwaysOnSampler, ParentBasedSampler, Sampler, SamplingResult} from "@ope
 import {Attributes, Context, createTraceState, diag, Link, SpanKind} from "@opentelemetry/api";
 import * as opentelemetry from "@opentelemetry/api";
 import {CoralogixAttributes, CoralogixTraceState} from "../common";
+import type express from 'express';
+import {ILayer} from "express-serve-static-core";
+import {SEMATTRS_HTTP_TARGET} from "@opentelemetry/semantic-conventions";
+
+export interface RouteMapping {
+    regex: RegExp,
+    path: string,
+}
+
+interface Stack {
+    route: { path: string },
+    regexp: RegExp
+}
+
+interface Handle {
+    stack?: Stack[],
+    __original?: { stack: Stack[] },
+}
+
+interface Handler {
+    handle: Handle,
+    name?: string,
+}
 
 export class CoralogixTransactionSampler implements Sampler {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     private readonly baseSampler: Sampler;
+    private routes: RouteMapping[] = [];
 
     constructor(baseSampler?: Sampler) {
         if (baseSampler) {
@@ -18,20 +41,70 @@ export class CoralogixTransactionSampler implements Sampler {
         }
     }
 
+
+    private _getPathFromRoutes(path: string): string | undefined {
+        return this.routes.find(route => route.regex.test(path))?.path;
+    }
+
+    private _buildTransactionNameFromExpressPath(path: string, spanName: string): string {
+        return `${spanName} ${path}`;
+    }
+
+    private _isMiddlewareILayer(middleware: Handler | ILayer): middleware is ILayer {
+        return "route" in middleware && !!middleware?.route;
+    }
+
+    private _isMiddlewareHandler(middleware: ILayer | Handler): middleware is Handler {
+        return middleware?.name === 'router';
+    }
+
+    setExpressApp(app: express.Application): void {
+        const routes: RouteMapping[] = [];
+
+        app._router.stack.forEach((middleware: Handler | ILayer) => {
+            if (this._isMiddlewareILayer(middleware)) {
+                // routes registered directly on the app
+                if (middleware.route?.path)
+                    routes.push({
+                        path: middleware.route.path,
+                        regex: middleware.regexp,
+                    });
+            } else if (this._isMiddlewareHandler(middleware)) {
+                // router middleware
+                const handle = middleware?.handle;
+                const stack = handle?.stack ?? handle?.__original?.stack;
+                stack && stack.forEach((handler) => {
+                    const route = handler.route;
+                    if (route) {
+                        routes.push({
+                            path: route.path,
+                            regex: handler.regexp,
+                        });
+                    }
+                });
+            }
+        });
+
+        this.routes = [...this.routes, ...routes];
+    }
+
     shouldSample(context: Context, traceId: string, spanName: string, spanKind: SpanKind, attributes: Attributes, links: Link[]): SamplingResult {
         const result = this.baseSampler.shouldSample(context, traceId, spanName, spanKind, attributes, links);
         try {
             const spanContext = opentelemetry.trace.getSpanContext(context);
 
-            // if distributed transaction exists, use it, if not this is the first span and thus the root of the distributed transaction
-            const distributedTransaction = spanContext?.traceState?.get(CoralogixTraceState.DISTRIBUTED_TRANSACTION_IDENTIFIER) ?? spanName;
+            const httpTarget = attributes?.[SEMATTRS_HTTP_TARGET]?.toString();
+            const path = this._getPathFromRoutes(httpTarget ?? '');
+
+            const transactionName = path ? this._buildTransactionNameFromExpressPath(path, spanName) : spanName;
+
+            const distributedTransaction = spanContext?.traceState?.get(CoralogixTraceState.DISTRIBUTED_TRANSACTION_IDENTIFIER) ?? transactionName;
 
             const existingTransaction = spanContext?.traceState?.get(CoralogixTraceState.TRANSACTION_IDENTIFIER);
 
-            // if span is remote, then start a new transaction, else try to use existing transaction
             const startsTransaction = existingTransaction === undefined || spanContext?.isRemote;
 
-            const transaction = startsTransaction ? spanName : existingTransaction;
+            const transaction = startsTransaction ? transactionName : existingTransaction;
 
             let {attributes: resultAttributes, traceState} = result;
             const {decision} = result;
@@ -45,17 +118,16 @@ export class CoralogixTransactionSampler implements Sampler {
                 [CoralogixAttributes.TRANSACTION_IDENTIFIER]: transaction,
                 [CoralogixAttributes.DISTRIBUTED_TRANSACTION_IDENTIFIER]: distributedTransaction,
                 [CoralogixAttributes.TRANSACTION_ROOT]: startsTransaction ?? undefined
-            }
+            };
 
             return {
                 decision,
                 attributes: resultAttributes,
                 traceState
-            }
+            };
         } catch (error) {
             diag.debug('CoralogixTransactionSampler failed, returning original sampler result', error);
             return result;
         }
     }
-
 }
